8070f5dccead16f077c7ee2613b7fe59a3c88a0e
AparnaGutta426 <aparna.gutta426@gmail.com>
2020-05-22 06:05:43 +0000

    dev feature

--- src/test/java/BuddyTool/demobuddy/AppTest.java
+++ src/test/java/BuddyTool/demobuddy/AppTest.java
@@ -4,40 +4,27 @@ import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
-/**
- * Unit test for simple App.
- */
 public class AppTest 
     extends TestCase
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
+    
     public AppTest( String testName )
     {
         super( testName );
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
+    
     public static Test suite()
     {
         return new TestSuite( AppTest.class );
     }
 
-    /**
-     * Rigourous Test :-)
-     */
+    
     public void testApp()
     {
         assertTrue( true );
         System.out.println("First commit");
-        System.out.println("Second commit");
-        
-        System.out.println("Development change 1");
-        System.out.println("Dev changes 2");
+        System.out.println("Second feature added");
+       
     }
 }
